a3585d85c0b1647ef9f1c22e769e37191860c6ed
Blackhacker-a <64885014+Blackhacker-a@users.noreply.github.com>
2020-05-16 12:34:05 +0700

    Em Optimize code

--- Bai 5.cpp
+++ Bai 5.cpp
@@ -4,22 +4,16 @@ int main() {
 	int a1 = 1, a2 = 1;//khoi tao gia tri dau
 	printf("Nhap vao so nguyen duong n ");
 	scanf("%d",&n);
-	if(n==1) {
-		printf("gia tri so Fibonacci nho hon so vua nhap la 0");
-	} else if(n==2) {
-		printf("gia tri so Fibonacci nho hon so vua nhap la 1");
-	} else {
 		int a;
-		
 		while(a1+a2<n){   
 				a = a1 + a2;
 				a1 = a2;
 				a2 = a;    // khoi tao gia tri cua so hang tu 3 tro di
 				}
-				
+		printf("So Fibonacci nho hon so vua nhap la %d",a2);//in ra giá tri truoc so a	la a2	
 		}
-		printf("So Fibonacci nho hon so vua nhap la %d",a2);//in ra giá tri truoc so a
-	}
+		
+	
 
 
 
