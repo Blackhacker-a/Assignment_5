cba41965baf0c54389158066fc730074468f13ec
Blackhacker-a <64885014+Blackhacker-a@users.noreply.github.com>
2020-05-16 17:31:14 +0700

    E optimize thuật toán

--- Bai 4.cpp
+++ Bai 4.cpp
@@ -1,14 +1,20 @@
 #include<stdio.h>
 int main(){
     int a,b;
-    do{ printf("Ta quy ve tim UCLN cua hai so nguyen duong la du\n ");
-	    printf("Nhap so a nguyen duong \n");
+    
+	    printf("Nhap so a nguyen  \n");
     	scanf("%d",&a);
-    	printf("Nhap so b nguyen duong \n");
+    	printf("Nhap so b nguyen \n");
     	scanf("%d",&b);
-    	} while(a<=0||b<=0);
+    	
    
     while(a!=b){
+    	if(a<0){
+		a=-a;
+		}
+    	if(b<0){
+		b=-b;
+		}
     	if(a>b){
     		a=a-b;
 		}
